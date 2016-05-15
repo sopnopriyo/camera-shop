b09e94fa6c124a9c7a5c4ef209ed3976f21243f7
sopnopriyo <sopnopriyo@gmail.com>
2016-05-15 16:33:58 +0800

    updating sql

--- secure_login.sql
+++ secure_login.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: May 07, 2016 at 04:18 PM
+-- Generation Time: May 15, 2016 at 04:31 PM
 -- Server version: 5.5.49-0ubuntu0.14.04.1
 -- PHP Version: 5.5.9-1ubuntu4.16
 
@@ -31,7 +31,7 @@ CREATE TABLE IF NOT EXISTS `catagory` (
   `name` varchar(200) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=19 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=22 ;
 
 --
 -- Dumping data for table `catagory`
@@ -40,7 +40,8 @@ CREATE TABLE IF NOT EXISTS `catagory` (
 INSERT INTO `catagory` (`id`, `name`) VALUES
 (1, 'DSLR'),
 (2, 'Lense'),
-(3, 'SLR');
+(3, 'SLR'),
+(4, 'Tripod');
 
 -- --------------------------------------------------------
 
@@ -65,7 +66,12 @@ INSERT INTO `login_attempts` (`user_id`, `time`) VALUES
 (2, '1462546246'),
 (2, '1462549117'),
 (2, '1462553234'),
-(2, '1462554544');
+(2, '1462554544'),
+(2, '1462863874'),
+(2, '1462863889'),
+(2, '1462873398'),
+(2, '1463241132'),
+(2, '1463298582');
 
 -- --------------------------------------------------------
 
@@ -87,8 +93,30 @@ CREATE TABLE IF NOT EXISTS `members` (
 --
 
 INSERT INTO `members` (`id`, `username`, `email`, `password`, `salt`) VALUES
-(1, 'test_user', 'test@example.com', '00807432eae173f652f2064bdca1b61b290b52d40e429a7d295d76a71084aa96c0233b82f1feac45529e0726559645acaed6f3ae58a286b9f075916ebf66cacc', 'f9aab579fc1b41ed0c44fe4ecdbfcdb4cb99b9023abb241a6db833288f4eea3c02f76e0d35204a8695077dcf81932aa59006423976224be0390395bae152d4ef'),
-(2, 'admin', 'sopnopriyo@gmail.com', '44e30430b04b12248f36276fbe9f7f3ed94caca5ec8eb3c22be40c74542c16f153a70c53bce756ea5d328193acc133afa3c72a3dade0105ef59bf427ca2659f8', '36b0fb1e1f0112d58f9fbc3821cf4d890991cfe66012e77c2cfba772e576df5573f7711ddcfb3c29a396cd7c4bd9fdcdf9a8a2032192dbc6f6ac7a816173fab8');
+(1, 'test_user', 'test@example.com', '00807432eae173f652f2064bdca1b61b290b52d40e429a7d295d76a71084aa96c0233b82f1feac45529e0726559645acaed6f3ae58a286b9f075916ebf66cacc', 'f9aab579fc1b41ed0c44fe4ecdbfcdb4cb99b9023abb241a6db833288f4eea3c02f76e0d35204a8695077dcf81932aa59006423976224be0390395bae152d4ef');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `message`
+--
+
+CREATE TABLE IF NOT EXISTS `message` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `phone` varchar(50) NOT NULL,
+  `message` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `message`
+--
+
+INSERT INTO `message` (`id`, `name`, `email`, `phone`, `message`) VALUES
+(1, 'Shahin', 'sopnopriyo@gmail.com', '0122617120', 'Hello . I have a fucking problem.'),
+(2, 'sopnopriyo', 'asa', 'asa', 'asasasaas');
 
 -- --------------------------------------------------------
 
@@ -106,7 +134,7 @@ CREATE TABLE IF NOT EXISTS `product` (
   `catagory_id` int(11) NOT NULL,
   `description` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
 
 --
 -- Dumping data for table `product`
@@ -114,7 +142,7 @@ CREATE TABLE IF NOT EXISTS `product` (
 
 INSERT INTO `product` (`id`, `name`, `price`, `image`, `color`, `stock_status`, `catagory_id`, `description`) VALUES
 (1, 'Canon 5D', 3000, 'link', 'Red', 'in stock', 2, 'This is a nice camera'),
-(2, 'Canon 5D', 3000, 'link', 'Red', 'in stock', 2, 'This is a nice camera');
+(2, 'beer', 400, 'uploads/13230312_1647580348900405_6662595471461734170_n.jpg', 'Blue', 'in stovk', 2, 'asasasasasasa');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
