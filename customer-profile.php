e5bd2f9b52b0d796d8a2b19a4162e4dd6e671975
dayahrazak <dayahrazak34@gmail.com>
2016-05-17 02:47:44 +0800

    Update customer-profile.php

--- customer-profile.php
+++ customer-profile.php
@@ -1,63 +1,54 @@
+ <?php
+
+include_once 'includes/db_connect.php';
+include_once 'includes/functions.php';
+
+sec_session_start();
+
+if (login_check($mysqli) == true) {
+    $logged = 'in';
+} else {
+    $logged = 'out';
+}
+?>
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Welcome to Virtual Eye Shop !</title>
-    <link rel="stylesheet" type="text/css" href="frontEnd.css">
-</head>
-<body>
-
-<header>
-<img src="img/marvel.png" alt="logo">
-</header>
-
-<?php 
-
-    include('main-menu.php')
-?>
-<body>
-
-<style type="text/css">
-
-</style>
-
-<div id="ProfilePage">
-    <div id="LeftCol">
-        <div id="Photo">
-
-        <img src="img/profile.png" height="200px" width="200px">
-        </div>
-        <div id="ProfileOptions">
-        Sirjon
+    <head>
+        <title>Secure Login: Log In</title>
+        <link rel="stylesheet" href="styles/main.css" />
+        <script type="text/JavaScript" src="js/sha512.js"></script> 
+        <script type="text/JavaScript" src="js/forms.js"></script> 
+    </head>
+    <body style="background-color: #0ca3d2;">
+        <section class="container">
+            <div class="login">
+              
+                    <?php
+                    if (isset($_GET['error'])) {
+                        echo '<p class="error">Error Logging In!</p>';
+                    }
+                    ?>
+<div class="container">
+    <div class="row">
+      <div class="main">
+          <h3 style="color:#65aeee;">Please Log In or <a href="userRegister.php">Sign Up</a></h3>
+          
+           <form action="includes/process_login.php" method="post" name="login_form"> 			
+                        <p> <input type="text" name="email" placeholder="Enter email" /> </p>
+                        <p> <input type="password" 
+                                         name="password" 
+                                         id="password" placeholder="Enter Password" /></p>
+                        <p><input type="button" 
+                               value="Login" 
+                               onclick="formhash(this.form, this.form.password);" /> </p>
+                    </form>
+                    <p>You are currently logged <?php echo $logged ?>.</p>
+              
+            </div>
+        </section>
+   
+              
+          </form>
         </div>
     </div>
-
-    <div id="Info">
-        <p>
-            <strong>Name:</strong>
-            <span>Sirjon</span>
-        </p>
-        <p>
-            <strong>email:</strong>
-            <span>abc@example.com</span>
-        </p>
-        <p>
-            <strong>phone:</strong>
-            <span>+6012222222</span>
-        </p>
-        <p>
-            <strong>Address:</strong>
-            <span>ababa,m ahsaha, ahah , hahahha ,lahahhaha, jakl</span>
-        </p>
-        <p>
-            <strong>Member Since:</strong>
-            <span>2009</span>
-        </p>
-    </div>
-
-    <!-- Needed because other elements inside ProfilePage have floats -->
-    <div style="clear:both"></div>
-</div>
-
-
-</body>
-</html>
+</div> 
