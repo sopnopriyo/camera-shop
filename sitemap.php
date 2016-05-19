156fabb593c69f91b8fa383ae94514b9333b3f32
sopnopriyo <sopnopriyo@gmail.com>
2016-05-19 23:38:31 +0800

    adding sitemap

--- sitemap.php
+++ sitemap.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Contact Us</title>
+    <title>Site Map</title>
+
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="frontEnd.css"> 
 </head>
 <body>
@@ -20,26 +22,33 @@
 <h2>Site Map</h2>
 
 <dl>
-    <dt>Home</dt>
-        <dd>- Camera </dd>
-        <dd>- Lense </dd>
-        <dd>- Memory Card </dd>
-        <dd>- Camera Bag</dd>
-        <dd>- Accesories</dd>
-    <dt>Camera</dt>
-        <dd>- DSLR </dd>
-        <dd>- Digital Camera </dd>
-        <dd>- Camrecorder </dd>
-        <dd>- Broadcast Camera </dd>
-    <dt>Accesories</dt>
-        <dd>- Lense</dd>
-        <dd>- Flash </dd>
-        <dd>- Tripod</dd>
-        <dd>- Memory Card </dd>
-        <dd>- Bag and Case</dd>
-        <dd>- Accesories</dd>
-    <dt>About Us</dt>
-    <dt>Contact Us</dt>
+     <ul>
+        <li><a href="index.php" >Home</a></li>
+<ul>
+        <li><a href="camera.php" >Camera</a></li>
+        <li><a href="lense.php" >Lense</a></li>
+<li><a href="accesories.php" >Accesories</a></li>
+    </ul>
+        <li><a href="camera.php">Camera</a></li>
+<ul>
+        <li><a href="dslr.php" >DSLR</a></li>
+        <li><a href="digital-camera.php" >Digital Camera</a></li>
+<li><a href="camrecorder.php" >Camrecorder</a></li>
+<li><a href="dslr.php" >Broadcast Camera</a></li>
+    </ul>
+        <li><a href="accesories.php" ="delete">Accesories</a></li>
+<ul>
+        <li><a href="lense.php" >Lense</a></li>
+        <li><a href="flash.php" >Flash</a></li>
+<li><a href="tripod.php" >Tripod</a></li>
+<li><a href="memorycard.php" >Memory Card</a></li>
+<li><a href="bagcase.php" >Bag and Case</a></li>
+<li><a href="accesories.php" ="delete">Accesories</a></li>
+
+    </ul>
+        <li><a href="about-us.php" ="check">About Us</a></li>
+        <li><a href="contact-us.php"="info">Contact Us</a></li>
+      </ul>
 </dl>
 
   
