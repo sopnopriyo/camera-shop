1c6f6808d27a8acb322e3d64fc41209fc4cec98f
Luqman Ahmad <luqmanahmaad@gmail.com>
2016-05-23 01:00:46 +0800

    dropdown nav bar

--- main-menu.php
+++ main-menu.php
@@ -2,11 +2,26 @@
 session_start();
    
  ?>
-<nav id="top-main-nav" style="font-family:tahoma">
-   <ul style="color:#301F22">
+<nav id="navbar" style="font-family:tahoma">
+   <ul>
         <li><a href="index.php">Home</a> </li>
-        <li><a href="camera.php">Camera</a> </li>
-        <li><a href="accesories.php">Accesories</a> </li>
+        <li><a href="camera.php">Camera</a>   
+            <ul>        
+                <li><a href="dslr.php">DSLR</a></li>
+                <li><a href="digital-camera.php">Digital Camera</a></li>
+                <li><a href="camrecorder.php">Camrecorder</a></li>
+                <li><a href="dslr.php">Broadcast Camera</a></li> 
+            </ul>           
+        </li>
+        <li><a href="accesories.php" >Accesories</a> 
+            <ul>
+                <li><a href="lense.php">Lense</a></li>
+                <li><a href="flash.php">Flash</a></li>
+                <li><a href="tripod.php">Tripod</a></li>
+                <li><a href="memorycard.php">Memory Card</a></li>
+                <li><a href="bagcase.php">Bag and Case</a></li>
+            </ul>
+        </li>
         <li><a href="about-us.php">About Us</a> </li>
         <li><a href="contact-us.php">Contact Us</a> </li>
         <li><a href="sitemap.php">Sitemap</a> </li>
@@ -14,7 +29,7 @@ session_start();
 
         <?php if (empty($_SESSION["name"])) {
             ?>
-             <li class="right"> <a href="customer-login.php"> <img src="img/user-icon.png"  height="20px" width="20px"> Login / Register</a></li>
+             <li style="float:right;"> <a href="customer-login.php"> <img src="img/user-icon.png"  height="16.5px" width="16.5px"> Login / Register</a></li>
             <?php
                 
                 }
@@ -22,7 +37,7 @@ session_start();
 
                     ?>
 
-                     <li class="right"> <a href="customer-profile.php"> <img src="img/user-icon.png"  height="20px" width="20px">
+                     <li style="float:right;"> <a href="customer-profile.php"> <img src="img/user-icon.png"  height="16.5px" width="16.5px">
 
                       <?php
                         echo "Hello ! ".$_SESSION["name"];
@@ -30,13 +45,13 @@ session_start();
                     ?>
                     </a></li>
 
-                    <li class="right"><a href="customer-logout.php">Logout</a> </li>
+                    <li style="float:right;"><a href="customer-logout.php">Logout</a> </li>
 
                     <?php
                 }
         ?>
          
-        <li class="right"> <a href="shopping-cart.php"> <img src="img/cart-icon.png"  height="20px" width="20px"> Cart (<?php  echo count($_SESSION['cart']);?> )</a></li>
+        <li style="float:right;" > <a href="shopping-cart.php"> <img src="img/cart-icon.png"  height="17px" width="17px"> Cart </a></li>
     </ul>
 
 </nav>
