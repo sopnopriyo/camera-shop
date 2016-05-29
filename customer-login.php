500b687cd814245a36e9f82f6a2274f1c4719b7f
Shahin Alam <sopnopriyo@gmail.com>
2016-05-28 20:26:29 -0700

    improving customer input validation

--- customer-login.php
+++ customer-login.php
@@ -7,41 +7,51 @@ include_once 'includes/db_connect.php';
    
    if($_SERVER["REQUEST_METHOD"] == "POST") {
       // username and password sent from form 
-      
-      $email = $_POST['email'];
-      $password = $_POST['password']; 
-      
-      
-      $sql = "SELECT id,name FROM customer WHERE email = '$email' and password = '$password'";
-      $result = $mysqli->query($sql);
+
+      $error = "";
 
 
-         while($row = mysqli_fetch_array($result))
-         {
-            $_SESSION['name'] =$row['name'];
-            $_SESSION['id'] =$row['id'];
-               
+      if (empty($_POST['email'])) {
+         $error = "name requred"; 
+      }
+      
+      if (empty($_POST['password'])) {
+         $error = "Password requred"; 
+      }
+      if (empty($error)) {
+         
+         $email = $_POST['email'];
+         $password = $_POST['password']; 
+         
+         
+         $sql = "SELECT id,name FROM customer WHERE email = '$email' and password = '$password'";
+         $result = $mysqli->query($sql);
+
+
+            while($row = mysqli_fetch_array($result))
+            {
+               $_SESSION['name'] =$row['name'];
+               $_SESSION['id'] =$row['id'];
+                  
+            }
+
+
+         $count = $result->num_rows;
+
+
+        
+         if($count == 1) {
+            
+           
+            header('Location: ./index.php');
+            
+            exit();
+                   
+         }else {
+            $error = "Your Login Name or Password is invalid";
          }
 
-
-      $count = $result->num_rows;
-
-
-     
-      // If result matched $myusername and $mypassword, table row must be 1 row
-		
-      if($count == 1) {
-         
-         //session_register("email");
-         //$_SESSION['login_user'] = $email;
-         
-         header('Location: ./index.php');
-         
-         exit();
-                
-      }else {
-         $error = "Your Login Name or Password is invalid";
-      }
+       }
       
    }
 ?>
